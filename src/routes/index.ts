9387e926b40873876640fa18fbea0bc69b6f71a5
prosper20 <bobsonconnect@gmail.com>
2024-10-13 21:27:44 +0100

    chore(routes): update Access-Control-Allow-Methods

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -26,6 +26,12 @@ class Routes {
       } else {
         res.setHeader("Access-Control-Allow-Origin", "*");
       }
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      res.setHeader("Access-Control-Allow-Credentials", "true");
       next();
     });
 
